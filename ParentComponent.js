fea41bc809d68deea5ebd6946c0e101960631359
aniket-mahadik-17 <92512266+aniket-mahadik-17@users.noreply.github.com>
2021-11-24 12:32:52 +0530

    Add files via upload

---
+++ ParentComponent.js
@@ -0,0 +1,27 @@
+// import React,{Component} from "react";
+// import ChildComponent from "./ChildComponent";
+
+// class ParentComponent extends Component{
+//     render(){
+//         state={
+//             balance:0,
+//         };
+//         balanceIncrement=(value)=>{
+//             this.setState({balance: this.state.balance + value});
+//         }
+//         const{name}=this.props;
+    
+//         return( <> 
+
+//         <h1>{name}</h1>
+//         <button >Family Income</button>
+//         <ChildComponent name={"Aniket"}/>
+//         <ChildComponent name={"Shubham"}/>
+       
+     
+//         </>
+//         );
+//     }
+
+// }
+// export default ParentComponent;
