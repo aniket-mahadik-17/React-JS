691591656ad0ab8dd9156f8fca75bd21123a265f
aniket-mahadik-17 <92512266+aniket-mahadik-17@users.noreply.github.com>
2021-11-24 12:33:51 +0530

    Add files via upload

--- FormComponent.js
+++ FormComponent.js
@@ -1,39 +1,20 @@
 import React,{Component} from "react";
 
+
 export default class FormComponent extends Component{
     state={
         name:"",
         age:0,
     };
-    // onInputNameChange=(event)=>{
-    //     this.setState({name:event.target.value})
-    // }
-    // onInputAgeChange=(event)=>{
-    //     this.setState({age:event.target.value})
-    // }
-    onInputChange=(event)=>{
-        this.setState({[event.target.name]:event.target.value})
-        console.log(event.target.name);
-    }
-
-    onFormSubmitHandler=(event)=>{
-        event.preventDefault();
-        console.log("in submit");
-        console.log(this.state);
-    }
     render(){
         return(
             <>
             <h1>Person Form</h1>
-            <form onSubmit={this.onFormSubmitHandler}>
             <label>Name:</label>
-            <input name={"name"} value={this.state.name} onChange={this.onInputChange}/>
+            <input/>
             <br/>
-            <label>Age:</label>
-            <input name={"age"} value={this.state.age} onChange={this.onInputChange}/>
-            <br/>
-            <button type="submit">Submit</button>
-            </form>
+            <label>Age::</label>
+            <input/>
             </>
         )
     }
