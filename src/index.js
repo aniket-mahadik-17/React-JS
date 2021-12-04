e56b8df70e55833cea71a55c6a9f53069850aedf
aniket-mahadik-17 <92512266+aniket-mahadik-17@users.noreply.github.com>
2021-12-04 17:50:43 +0530

    Nine-app

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './Store';
 
 
 ReactDOM.render(
-  <BrowserRouter>
   <React.StrictMode>
+    <Provider store={store}>
     <App />
-  </React.StrictMode>
-  </BrowserRouter>,
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
