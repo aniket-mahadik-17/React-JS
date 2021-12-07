d1876ff6c3936ce243d4155924d2b2578e47dfbe
aniket-mahadik-17 <92512266+aniket-mahadik-17@users.noreply.github.com>
2021-12-07 17:48:20 +0530

    Hooks-app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Counter from './Counter';
-import NewCounter from './NewCounter';
-import FunctionaComponent from './FunctionaComponent';
-import OnlineOrders from './OnlineOrders';
-import OfflineOrders from './OfflineOrders';
+import CricketCareer from './CricketCareer';
+import IndianAward from './IndianAward';
 
 function App() {
   return (
     <div className="App">
-      <OnlineOrders/>
-      <OfflineOrders/>
-      {/* <FunctionaComponent/> */}
-      {/* <Counter/> */}
-      {/* <NewCounter/> */}
+      <CricketCareer/>
+      <IndianAward/>
     </div>
   );
 }
