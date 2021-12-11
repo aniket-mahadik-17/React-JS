ca352782ecc2281f499f6d5899517423fc1183b8
aniket-mahadik-17 <92512266+aniket-mahadik-17@users.noreply.github.com>
2021-12-11 17:32:27 +0530

    CRUD'adv using hooks

--- src/HooksSampleEdit.js
+++ src/HooksSampleEdit.js
@@ -3,10 +3,18 @@ import axios from "axios";
 
 export default ()=>{
     const [postList, setPostList]= useState([]);
-    const [title, setTitle] = useState("");
-    const [body, setBody] = useState("");
+    // const [title, setTitle] = useState("");
+    // const [titleEditMode, setTitleEditMode] = useState("");
+    const [editObject, setEditObject] = useState({});
+    // const [body, setBody] = useState("");
+    const [postObject, setPostObject] =useState({
+        title:"",
+        body:"",
+    });
+    const [bodyEditMode, setBodyEditMode] = useState("");
     const [isEditMode, setIsEditMode] = useState(false);
     const [id, setId] = useState(0);
+    const [selectedPostId, setSelectedPostId] =useState(-1);
 
 
     useEffect(()=>{
@@ -22,38 +30,78 @@ export default ()=>{
             }
         })
     };
-    const onTitleChange =(event)=>{
-        setTitle(event.target.value);
+    // const onTitleChange =(event)=>{
+    //     setTitle(event.target.value);
+    // }
+    // const onTitleEditModeChange =(event)=>{
+    //     setTitleEditMode(event.target.value);
+    // }
+    // const onBodyChange =(event)=>{
+    //     setBody(event.target.value);
+    // }
+    // const onBodyEditModeChange =(event)=>{
+    //     setBodyEditMode(event.target.value);
+    // }
+    const onPostObjectChangeHandler=(event)=>{
+        if(event){
+            const {name, value}=event.target;
+            setPostObject({
+                ...postObject,
+                [name]:value,
+            });
+        }
+
     }
-    const onBodyChange =(event)=>{
-        setBody(event.target.value);
-    }
+    const onEditObjectChangeHandler=(event)=>{
+        // const titleOld={
+        //     title:"a",
+        //     body:"b",
+        // }
+        // const titleNew={
+        //     title:titleOld.title,
+        //     body:titleOld.body,
+        //     ...titleOld,
+        //     title:"c",
+        // }
+        if(event){
+            const {name, value}=event.target;
+            setEditObject({
+                ...editObject,
+                [name]:value,
+            });
+        }
+    } 
     const onFormSubmit=(event)=>{
         event.preventDefault();
-        console.log(title, body);
+        // console.log(title, body);
 
         if(!isEditMode){
-            axios.post("https://jsonplaceholder.typicode.com/posts",{title,body, userId:1,}).then((response)=>{
+            axios.post("https://jsonplaceholder.typicode.com/posts",{...postObject, userId:1,}).then((response)=>{
                 console.log(response);
                 alert("Post Added");
                 //fetch data again
                 fetchData();
                 //reset form
-                setTitle("");
-                setBody("");
+                // setTitle("");
+                // setBody("");
+                setPostObject({
+                    title:"",
+                    body:"",
+                })
             })
-        }else{
-            if(id>0){
-                axios.put("https://jsonplaceholder.typicode.com/posts/"+ id, {title, body,id:1 }).then((response)=>{
-                if(response){
-                    console.log(response);
-                    fetchData();
-                    alert("Post Updated");
-                    onResetClickHandler();
-                }
-            })
-            }
         }
+        // else{
+        //     if(id>0){
+        //         axios.put("https://jsonplaceholder.typicode.com/posts/"+ id, {title, body,id:1 }).then((response)=>{
+        //         if(response){
+        //             console.log(response);
+        //             fetchData();
+        //             alert("Post Updated");
+        //             onResetClickHandler();
+        //         }
+        //     })
+        //     }
+        // }
 
        
     }
@@ -65,30 +113,63 @@ export default ()=>{
         })
     }
     const onEditClickHandler=(postObject)=>{
-        setIsEditMode(true);
-        setTitle(postObject.title);
-        setBody(postObject.body);
+        // setIsEditMode(true);
+        // setTitle(postObject.title);
+        // setTitleEditMode(postObject.title);
+        // setBody(postObject.body);
+       
+        setEditObject({
+            title:postObject.title,
+            body:postObject.body, 
+        });
         setId(postObject.id);
+        setSelectedPostId(postObject.id)
+    }
+    const onResetEditHandler=()=>{
+        setSelectedPostId(-1);
+        setPostObject({
+            title:"",
+            body:"",
+        })
+    }
+    const onUpdateRowHandler=()=>{
+            if(id>0){
+                axios.put("https://jsonplaceholder.typicode.com/posts/"+ id, {...editObject,id:1 }).then((response)=>{
+                if(response){
+                    console.log(response);
+                    fetchData();
+                    alert("Post Updated");
+                    // reset state again
+                    onResetEditHandler();                        
+                    
+                }
+            })
+            }
+
     }
     const onResetClickHandler=(event)=>{
         if(event){
             event.preventDefault();
         }
         setIsEditMode(false);
-        setTitle("");
-        setBody("");
+        setPostObject({
+            title:"",
+            body:"",
+        })
+        // setTitle("");
+        // setBody("");
 
     }
 
     return(
         <>
-        <h1>Data From Post</h1>
+        <h1>Data From Post</h1> 
         <h2>Post Form</h2>
         <form onSubmit={onFormSubmit}>
             <label>Title</label>
-            <input name="title" value={title} onChange={onTitleChange}></input>
+            <input name="title" value={postObject.title} onChange={onPostObjectChangeHandler}></input>
             <label>Body</label>
-            <input name="body" value={body} onChange={onBodyChange}></input>
+            <input name="body" value={postObject.body} onChange={onPostObjectChangeHandler}></input>
             <button type="submit">{isEditMode? "Update":"Submit"}</button>
             {isEditMode && <button onClick={onResetClickHandler}>Cancel</button>}
         </form>
@@ -102,14 +183,19 @@ export default ()=>{
                 </tr>
             </thead>
             <tbody>
-               {postList.map((post, index)=>{
+                {postList.map((post, index)=>{
                    return(
                     <tr key={post.id}>
                     <td>{post.id}</td>
-                    <td>{post.title}</td>
-                    <td>{post.body}</td>
-                    <td><button onClick={()=>{onDeleteClickHandler(post.id)}}>Delete</button></td>
-                    <td><button onClick={()=>{onEditClickHandler(post)}}>Edit</button></td>
+                    <td>{selectedPostId===post.id? <input name="title" value={editObject.title} onChange={onPostObjectChangeHandler}/>:post.title}</td>
+                    <td>{selectedPostId===post.id? <input name="body" value={editObject.body} onChange={onPostObjectChangeHandler}/>:post.body}</td>
+
+                    <td>{selectedPostId===post.id ?(<button onClick={onUpdateRowHandler}>Update</button>
+                    ):(<><button onClick={()=>{onDeleteClickHandler(post.id)}}>Delete</button></>
+                    )}</td>
+
+                    <td>{selectedPostId===post.id ?(<button onClick={onResetEditHandler}>Reset</button>
+                    ):(<><button onClick={()=>{onEditClickHandler(post)}}>Edit</button></>)}</td>
                     </tr>
                    )
                })}
